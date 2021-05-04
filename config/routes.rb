eb5d190f4530c2aea18d2550732fbc0ae8e0a29d
maitakesann <kankore1204a@yahoo.co.jp>
2021-05-04 06:48:49 +0000

    いいね追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
- 
+
   devise_for :users
   root 'homes#top'
   get 'home/about' => 'homes#about'
-  
+
   resources :users,only: [:show,:index,:edit,:update]
-  resources :books
+  resources :books do
+  resource :favorites, only: [:create, :destroy]
+  end
 end
