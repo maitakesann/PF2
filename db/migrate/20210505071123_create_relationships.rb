caed1b0b0868208ef774ed61a32e4347bec81791
maitakesann <kankore1204a@yahoo.co.jp>
2021-06-16 11:27:06 +0000

    relationship migrate 後

--- db/migrate/20210505071123_create_relationships.rb
+++ db/migrate/20210505071123_create_relationships.rb
@@ -1,12 +1,16 @@
 class CreateRelationships < ActiveRecord::Migration[5.2]
-  # def change
-  #   # create_table :relationships do |t|
-  #   #   t.references :user, foreign_key: true
-  #   #   t.references :follow, foreign_key: { to_table: :users }
+   def change
+      create_table :relationships do |t|
+        t.references :user, foreign_key: true
+        t.references :follow, foreign_key: { to_table: :users }
+        
+        t.integer :follower_id
+        t.integer :following
 
-  #   #   t.timestamps
+        t.timestamps
+        
       
-  #   #   t.index [:user_id, :follow_id], unique: true
-  #   end
-  # end
+       t.index [:user_id, :follow_id], unique: true
+     end
+   end
 end
--- db/migrate/20210506034125_add_following_id_to_relationships.rb
+++
@@ -1,5 +0,0 @@
-class AddFollowingIdToRelationships < ActiveRecord::Migration[5.2]
-  def change
-    add_column :relationships, :following_id, :integer
-  end
-end
