7bf563ae12858515cb67608cedb1b2819824f44f
maitakesann <kankore1204a@yahoo.co.jp>
2021-07-12 06:35:54 +0000

    [Update]book contr @tag_list~ ,top delete

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -24,6 +24,7 @@ class BooksController < ApplicationController
       redirect_to book_path(@book), notice: "You have created book successfully."
     else
       @books = Book.all
+      @tag_list = Tag.all
       render 'index'
     end
   end
